e1f957003ba898776660b9e766a2d79b9a671ed2
Wooksang Ryu <today8934@gmail.com>
2022-07-23 00:29:26 +0900

    프로젝트 기본세팅

---
+++ src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
@@ -0,0 +1,13 @@
+package io.security.corespringsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+    @GetMapping(value = "/messages")
+    public String myPage() {
+        return "user/messages";
+    }
+}
